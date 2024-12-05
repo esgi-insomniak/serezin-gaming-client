f57f428408ab702d784fad2c74e7f44ab803d1ab
Loan CLERIS <loan.cleris@gmail.com>
2024-12-05 20:31:04 +0100

    refactor(layout content): update display to render items

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -5,34 +5,27 @@ import {
   SidebarGroupContent,
   SidebarGroupLabel
 } from '@/components/ui/sidebar';
-import { ClientRoutes, getPath } from '@/router';
 import { LayoutSidebarHeader } from '@/components/layout/_components/sidebar-header.tsx';
 import { LayoutSidebarFooter } from '@/components/layout/_components/sidebar-footer.tsx';
 import { LayoutSidebarContent } from '@/components/layout/_components/sidebar-content.tsx';
-import dynamicIconImports from 'lucide-react/dynamicIconImports';
+import { sidebarGroup } from '@/components/layout/utils';
 
 export function AppSidebar() {
-  const items: {
-    title: string;
-    icon: keyof typeof dynamicIconImports;
-    url: string;
-  }[] = [
-    {
-      title: 'Dashboard',
-      icon: 'house',
-      url: getPath(ClientRoutes.HOME)
-    }
-  ];
   return (
     <Sidebar>
       <LayoutSidebarHeader />
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <LayoutSidebarContent items={items} />
-          </SidebarGroupContent>
-        </SidebarGroup>
+        {Object.keys(sidebarGroup).map((category) => {
+          const items = sidebarGroup[category];
+          return (
+            <SidebarGroup key={category}>
+              <SidebarGroupLabel>{category}</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <LayoutSidebarContent items={items} />
+              </SidebarGroupContent>
+            </SidebarGroup>
+          );
+        })}
       </SidebarContent>
       <LayoutSidebarFooter />
     </Sidebar>
---
+++ src/components/layout/utils/sidebar-group.ts
@@ -0,0 +1,37 @@
+import dynamicIconImports from 'lucide-react/dynamicIconImports';
+import { ClientRoutes, getPath } from '@/router';
+
+type SidebarGroupType = {
+  [key: string]: SingleSidebarGroupType[];
+};
+
+type SingleSidebarGroupType = {
+  title: string;
+  icon: keyof typeof dynamicIconImports;
+  url: string;
+  visible?: boolean;
+};
+
+export const sidebarGroup: SidebarGroupType = {
+  APPLICATION: [
+    {
+      title: 'Dashboard',
+      icon: 'house',
+      url: getPath(ClientRoutes.HOME)
+    }
+  ],
+  TOURNOI: [
+    {
+      title: 'Predictions',
+      icon: 'spade',
+      url: getPath(ClientRoutes.HOME)
+    }
+  ],
+  ADMIN: [
+    {
+      title: 'Devenir admin',
+      icon: 'shield',
+      url: getPath(ClientRoutes.HOME)
+    }
+  ]
+};
