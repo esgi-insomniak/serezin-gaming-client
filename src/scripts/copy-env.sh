a7e265cf6c34dbfcece45aa1ca3fa16173701f57
Loan CLERIS <loan.cleris@gmail.com>
2024-12-05 11:10:32 +0100

    feat(env): add scripts to always have the right variables

---
+++ src/scripts/copy-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "RIOT_API_TOKEN=$RIOT_API_TOKEN" >> .env
+echo "GITHUB_URL=$GITHUB_URL" >> .env
+echo "BRANCH=$BRANCH" >> .env
+echo "API_FOLDER_PATH=$API_FOLDER_PATH" >> .env
+echo "ZIP_FILE=$ZIP_FILE" >> .env
