c8a9bb4ecd1c206db230f44e49a521dffade54ef
Loan CLERIS <loan.cleris@gmail.com>
2024-12-06 21:30:43 +0100

    feat(header): select tournament

--- src/components/layout/_components/sidebar-header.tsx
+++ src/components/layout/_components/sidebar-header.tsx
@@ -1,15 +1,35 @@
 import {
   DropdownMenu,
-  DropdownMenuContent
+  DropdownMenuContent,
+  DropdownMenuItem
 } from '@/components/ui/dropdown-menu';
 import { SidebarHeader, SidebarMenuButton } from '@/components/ui/sidebar';
 import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 import { Archive, ChevronsUpDown, Crown } from 'lucide-react';
-import { useTournamentFindAll } from '@/libs/api';
+import { TournamentDto, useTournamentFindAll } from '@/libs/api';
+import { useCallback, useState } from 'react';
+import { ClientRoutes, useHandleRedirection } from '@/router';
+import { Separator } from '@/components/ui/separator.tsx';
 
 export function LayoutSidebarHeader() {
   const { data: { data: { data: allTournaments = [] } = {} } = {} } =
     useTournamentFindAll();
+
+  const { redirect } = useHandleRedirection();
+
+  const [currentTournament, setCurrentTournament] =
+    useState<TournamentDto | null>(null);
+
+  const handleTournamentChange = useCallback(
+    (tournament: TournamentDto) => {
+      setCurrentTournament(tournament);
+      redirect(ClientRoutes.HOME, { params: { tournamentId: tournament.id } });
+    },
+    [redirect]
+  );
+
+  const Icon = currentTournament?.isArchived ? Archive : Crown;
+
   return (
     <SidebarHeader>
       <DropdownMenu>
@@ -21,8 +41,10 @@ export function LayoutSidebarHeader() {
               <Crown className="size-4" />
             </div>
             <div className="flex flex-col gap-0.5 leading-none">
-              <span className="font-semibold">Tournoi</span>
-              <span className="">Edition 2025</span>
+              <span className="font-semibold">
+                {currentTournament?.id ?? 'Selectionner un tournoi'}
+              </span>
+              <span className="">{currentTournament?.id ?? ''}</span>
             </div>
             <ChevronsUpDown className="ml-auto" />
           </SidebarMenuButton>
@@ -32,22 +54,22 @@ export function LayoutSidebarHeader() {
           align="start">
           <div className="flex flex-col gap-1 p-2">
             {allTournaments.map((tournament) => (
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  {tournament.isArchived ? (
-                    <Archive className="size-4" />
-                  ) : (
-                    <Crown className="size-4" />
-                  )}
-                </div>
+              <DropdownMenuItem
+                key={tournament.id}
+                onSelect={() => handleTournamentChange(tournament)}>
+                <Icon className="size-4" />
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="">{tournament.id}</span>
                 </div>
-                <ChevronsUpDown className="ml-auto" />
-              </SidebarMenuButton>
+              </DropdownMenuItem>
             ))}
+          </div>
+          <Separator className="my-2" />
+          <div className="p-2">
+            <DropdownMenuItem onSelect={() => setCurrentTournament(null)}>
+              <Crown className="size-4" />
+              Selectionner un tournoi
+            </DropdownMenuItem>
           </div>
         </DropdownMenuContent>
       </DropdownMenu>
