69f59dea0c2535e5e110a7aabd2653338511c0ab
Tom Cuillandre <tom.cuillandre@sfr.fr>
2024-12-10 16:11:48 +0100

    feat(authentication): cookie verification to define identity/connected

--- src/pages/private-routes/tournament/tournament.tsx
+++ src/pages/private-routes/tournament/tournament.tsx
@@ -1,10 +1,12 @@
+import { useTournamentGetOneById } from '@/libs/api';
 import { useParams } from 'react-router-dom';
-import { useTournamentFindOne } from '@/libs/api';
 
 export default function Tournament() {
   const { tournamentId = '' } = useParams<{ tournamentId: string }>();
   const { data: { data: { data: currentTournament } = {} } = {} } =
-    useTournamentFindOne(tournamentId, { query: { enabled: !!tournamentId } });
+    useTournamentGetOneById(tournamentId, {
+      query: { enabled: !!tournamentId }
+    });
   return (
     <div>
       <h1>Welcome to {currentTournament?.name ?? 'not found'}</h1>
