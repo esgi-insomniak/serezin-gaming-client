8a86a18244a1b7c2db7f16022fb9051479807ff8
Loan CLERIS <loan.cleris@gmail.com>
2024-12-10 23:48:27 +0100

    feat(home): update with background and input to search for players

--- src/pages/private-routes/home.tsx
+++ src/pages/private-routes/home.tsx
@@ -1,12 +1,26 @@
-import { Button } from '@/components/ui/button.tsx';
-import { useToaster } from '@/libs/providers';
+import AnimatedGridPattern from '@/components/ui/animated-grid-pattern';
+import { cn } from '@/libs/utils.ts';
+import { Input } from '@/components/ui/input.tsx';
 
 export default function Home() {
-  const toast = useToaster();
-
   return (
-    <div>
-      <Button onClick={() => toast?.success('test')}>Show Toast</Button>
+    <div className="relative flex h-full flex-col w-full items-center justify-center overflow-hidden rounded-lg bg-background p-20 md:shadow-xl">
+      <div className="flex flex-col space-y-5 justify-center items-center">
+        <p className="z-10 whitespace-pre-wrap text-center text-4xl font-medium tracking-tighter text-black dark:text-white uppercase">
+          Recherchez des summoners
+        </p>
+        <Input className="w-[30rem] h-14 " />
+      </div>
+      <AnimatedGridPattern
+        numSquares={30}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          '[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]',
+          'inset-x-0 inset-y-[-30%] h-[200%] skew-y-12'
+        )}
+      />
     </div>
   );
 }
